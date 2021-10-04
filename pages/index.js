932f4c5bf73ba59c86bdfedc8119311aad4646d3
Irene Oppo <i.oppo@datocms.com>
2021-10-04 09:55:15 +0200

    Moved script to the header

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,40 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import products from "../app/assets/products.json"
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import products from "../app/assets/products.json";
 import Product from "../app/components/Product/Product";
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>E-commerce</title>
-                <link rel="icon" href="/favicon.ico" />
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>E-commerce</title>
+        <link rel="icon" href="/favicon.ico" />
 
-                <link rel="preconnect" href="https://cdn.snipcart.com" />
-                <link
-                    rel="stylesheet"
-                    href="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.css"
-                />
-            </Head>
+        <link rel="preconnect" href="https://cdn.snipcart.com" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.css"
+        />
+        <script
+          src="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.js"
+          async
+        />
+      </Head>
 
-            <main className={styles.main}>
-                <h1>
-                    E-Commerce in Next.js and SnipCart
-                </h1>
+      <main className={styles.main}>
+        <h1>E-Commerce in Next.js and SnipCart</h1>
 
-                <div className={styles.grid}>
-                    {
-                        products.map((product, i) => <Product {...product} key={i}/>)
-                    }
-                </div>
-            </main>
-            <div
-                id="snipcart"
-                data-api-key="NWMwZWNkZGMtZjU2ZS00YzM3LWFlZjYtMmM5Zjk0MWViZDcxNjM3Njg0OTY0ODg5NTk4MTM3" hidden
-            >
-            </div>
-            <script
-                src="https://cdn.snipcart.com/themes/v3.2.1/default/snipcart.js"
-                async
-            />
+        <div className={styles.grid}>
+          {products.map((product, i) => (
+            <Product {...product} key={i} />
+          ))}
         </div>
-    );
+      </main>
+      <div
+        id="snipcart"
+        data-api-key="NWMwZWNkZGMtZjU2ZS00YzM3LWFlZjYtMmM5Zjk0MWViZDcxNjM3Njg0OTY0ODg5NTk4MTM3"
+        hidden
+      ></div>
+    </div>
+  );
 }
